1db74605e1aa98b9d3b73c56ad706c5c61f1271a
carol <caroline.ocortes@gmail.com>
2022-12-23 00:13:47 -0300

    start react-app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IoIosConstruct } from 'react-icons/io';
+import { AiOutlineCoffee } from 'react-icons/ai';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Portfólio em construção!</h1>
+      <div className="constructionIcons">
+        <span>...</span>
+        <AiOutlineCoffee />
+        <IoIosConstruct />
+      </div>
+    </div>
+  );
+}
+
+export default App;
